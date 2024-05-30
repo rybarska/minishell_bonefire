4016a3e4d6cb705e2135e5a230e9aad7addab566
Agata Rybarska <arybarsk@c2r1p1.42vienna.com>
2024-05-30 17:35:34 +0200

    Set up heredoc signals handling

--- signals.c
+++ signals.c
@@ -23,6 +23,12 @@ static void	handle_sigint(int signal_number)
 	rl_redisplay();
 }
 
+static void	heredoc_sigint(int signal_number)
+{
+	(void)signal_number;
+	g_o_on = 86;
+}
+
 // static void	handle_sigquit(int signal_number)
 // {
 // 	g_o_on = signal_number;
@@ -37,6 +43,8 @@ void	set_signal_controls(t_data *data)
 		signal(SIGINT, SIG_DFL);
 	else if (data->signal_mode == NON_INTERACTIVE)
 		signal(SIGINT, SIG_IGN);
+	else if (data->signal_mode == HERE_DOC)
+		signal(SIGINT, &heredoc_sigint);
 
 	if (data->signal_mode == INTERACTIVE)
 		signal(SIGQUIT, SIG_IGN);
@@ -44,32 +52,34 @@ void	set_signal_controls(t_data *data)
 		signal(SIGQUIT, SIG_DFL);
 	else if (data->signal_mode == NON_INTERACTIVE)
 		signal(SIGQUIT, SIG_IGN);
+	else if (data->signal_mode == HERE_DOC)
+		signal(SIGQUIT, SIG_IGN);
 }
 
-//void	set_signal_controls(t_data *data)
-//{
-	// struct sigaction	sa_int;
-	// struct sigaction	sa_quit;
+// void	set_signal_controls(t_data *data)
+// {
+// 	struct sigaction	sa_int;
+// 	struct sigaction	sa_quit;
 	
-	// sigemptyset(&sa_int.sa_mask);
-	// if (data->signal_mode == INTERACTIVE)
-	// 	sa_int.sa_handler = handle_sigint;
-	// else if (data->signal_mode == CHILD)
-	// 	sa_int.sa_handler = SIG_DFL;
-	// else if (data->signal_mode == NON_INTERACTIVE)
-	// 	sa_int.sa_handler = SIG_IGN;
-	// sa_int.sa_flags = SA_RESTART;
-	// if (sigaction(SIGINT, &sa_int, NULL) < 0)
-	// 	snuff_it(data, "Error: sigaction (sigint) failed\n", NULL, 255);
-	// sigemptyset(&sa_quit.sa_mask);
-	// if (data->signal_mode == INTERACTIVE || NON_INTERACTIVE)
-	// 	sa_quit.sa_handler = SIG_IGN;
-	// else if (data->signal_mode == CHILD)
-	// 	sa_quit.sa_handler = SIG_DFL; //TODO: print quit core dumped
-	// sa_quit.sa_flags = SA_RESTART;
-	// if (sigaction(SIGQUIT, &sa_quit, NULL) < 0)
-	// 	snuff_it(data, "Error: sigaction (sigquit) failed\n", NULL, 255);
-//}
+// 	sigemptyset(&sa_int.sa_mask);
+// 	if (data->signal_mode == INTERACTIVE)
+// 		sa_int.sa_sigaction = &handle_sigint;
+// 	else if (data->signal_mode == CHILD)
+// 		sa_int.sa_handler = SIG_DFL;
+// 	else if (data->signal_mode == NON_INTERACTIVE)
+// 		sa_int.sa_handler = SIG_IGN;
+// 	sa_int.sa_flags = SA_RESTART;
+// 	if (sigaction(SIGINT, &sa_int, NULL) < 0)
+// 		snuff_it(data, "Error: sigaction (sigint) failed\n", NULL, 255);
+// 	sigemptyset(&sa_quit.sa_mask);
+// 	if (data->signal_mode == INTERACTIVE || NON_INTERACTIVE)
+// 		sa_quit.sa_handler = SIG_IGN;
+// 	else if (data->signal_mode == CHILD)
+// 		sa_quit.sa_handler = SIG_DFL; //TODO: print quit core dumped
+// 	sa_quit.sa_flags = SA_RESTART;
+// 	if (sigaction(SIGQUIT, &sa_quit, NULL) < 0)
+// 		snuff_it(data, "Error: sigaction (sigquit) failed\n", NULL, 255);
+// }
 
 void	set_mode(t_data *data, t_signal_mode mode)
 {
