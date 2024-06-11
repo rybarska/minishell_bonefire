84e73529e2ab996e411bdc69c1b192cc8bb7eeb7
Maria Kamilla Huszar <mhuszar@c4r7p1.42vienna.com>
2024-06-11 21:08:15 +0200

    completely reworked asm so norminette doesnt cry cuz of volatile but the new version is also way more superior

--- hashing.c
+++ hashing.c
@@ -6,37 +6,56 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 15:25:17 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/06/11 19:32:42 by mhuszar          ###   ########.fr       */
+/*   Updated: 2024/06/11 21:05:24 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// size_t	get_hash(char *keyvalue)
+// {
+// 	size_t	hash;
+// 	size_t	counter;
+// 	size_t	hash2;
+// 	size_t	hashsize;
+
+// 	if (keyvalue == NULL)
+// 		return (0);
+// 	hashsize = HASHTABLE_SIZE;
+// 	hash = 0;
+// 	counter = 0;
+// 	hash2 = 0;
+// 	while (keyvalue[counter])
+// 	{
+// 		 __asm__ volatile ("movq $31, %%rdx; pushq %%rbx;"
+// 				"xorq %%rbx, %%rbx; movb %2, %%bl;"
+// 				"mulq %%rdx; addq %%rbx, %%rax;"
+// 				"divq %%rcx; popq %%rbx;"
+// 				: "=d"(hash2)
+// 				: "a"(hash), "r"(keyvalue[counter]), "c"(hashsize)
+// 				:
+//         );
+// 		hash = hash2;
+// 		counter++;
+// 	}
+// 	return (hash);
+// }
+
 size_t	get_hash(char *keyvalue)
 {
 	size_t	hash;
-	size_t	counter;
-	size_t	hash2;
-	size_t	hashsize;
 
-	if (keyvalue == NULL)
-		return (0);
-	hashsize = HASHTABLE_SIZE;
-	hash = 0;
-	counter = 0;
-	hash2 = 0;
-	while (keyvalue[counter])
-	{
-		__asm__ volatile ("movq $31, %%rdx; pushq %%rbx;"
-				"xorq %%rbx, %%rbx; movb %2, %%bl;"
-				"mulq %%rdx; addq %%rbx, %%rax;"
-				"divq %%rcx; popq %%rbx;"
-				: "=d"(hash2)
-				: "a"(hash), "r"(keyvalue[counter]), "c"(hashsize)
-				:);
-		hash = hash2;
-		counter++;
-	}
+	__asm__ volatile ("pushq %%rbx; lea (%1), %%rbx; xorq %%rdx, %%rdx;"
+		"1:"
+		"movb (%%rbx), %%al; cmpb %%al, %%al; jz 2f;"
+		"imulq $31, %%rdx; movzx %%al, %%rax;"
+		"addq %%rax, %%rdx; movl %2, %%eax; divq %%rdx;"
+		"incq %%rbx; jmp 1b;"
+		"2:"
+		"mov %%rdx, %0; popq %%rbx;"
+		: "=r"(hash)
+		: "r"(keyvalue), "r"(HASHTABLE_SIZE)
+		: "rax", "rdx");
 	return (hash);
 }
 
@@ -53,13 +72,13 @@ size_t	get_hash2(char *keyvalue)
 	hash2 = 0;
 	while (keyvalue[counter])
 	{
-		__asm__ volatile ("movq $17, %%rdx; pushq %%rbx;"
-				"xorq %%rbx, %%rbx; movb %2, %%bl;"
-				"mulq %%rdx; addq %%rbx, %%rax;"
-				"addq %%rcx, %%rax; popq %%rbx;"
-				: "=a"(hash2)
-				: "a"(hash), "r"(keyvalue[counter]), "c"(counter)
-				:);
+		__asm__ volatile("movq $17, %%rdx; pushq %%rbx;"
+							"xorq %%rbx, %%rbx; movb %2, %%bl;"
+							"mulq %%rdx; addq %%rbx, %%rax;"
+							"addq %%rcx, %%rax; popq %%rbx;"
+							: "=a"(hash2)
+							: "a"(hash), "r"(keyvalue[counter]), "c"(counter)
+							:);
 		hash = hash2;
 		counter++;
 	}
