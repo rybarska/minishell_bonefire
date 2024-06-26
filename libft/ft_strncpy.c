f3266ad398e3cd047b225ace2645dd6ca229125d
Agata Rybarska <arybarsk@c4r7p7.42vienna.com>
2024-06-26 18:20:48 +0200

    Fix norm issues

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -12,10 +12,11 @@
 
 #include "libft.h"
 
-char *ft_strncpy(char *s1, char *s2, int n)
+char	*ft_strncpy(char *s1, char *s2, int n)
 {
-	int i = -1;
+	int	i;
 
+	i = -1;
 	while (++i < n && s2[i])
 		s1[i] = s2[i];
 	s1[i] = '\0';
